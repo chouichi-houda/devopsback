255883a2c1695452355be330b6449cf57b76b122
chaima-kaddour <79282823+chaima-kaddour@users.noreply.github.com>
2022-11-08 15:02:04 +0100

    update secteur

--- src/main/java/com/esprit/examen/controllers/SecteurActiviteController.java
+++ src/main/java/com/esprit/examen/controllers/SecteurActiviteController.java
@@ -8,7 +8,6 @@ import com.esprit.examen.entities.SecteurActivite;
 import com.esprit.examen.services.ISecteurActiviteService;
 
 import io.swagger.annotations.Api;
-
 @RestController
 @Api(tags = "Gestion des secteurs activites")
 @RequestMapping("/secteurActivite")
@@ -22,8 +21,8 @@ public class SecteurActiviteController {
 	@GetMapping("/retrieve-all-secteurActivite")
 	@ResponseBody
 	public List<SecteurActivite> getSecteurActivite() {
-		List<SecteurActivite> list = secteurActiviteService.retrieveAllSecteurActivite();
-		return list;
+		return secteurActiviteService.retrieveAllSecteurActivite();
+
 	}
 
 	// http://localhost:8089/SpringMVC/secteurActivite/retrieve-secteurActivite/8
@@ -38,9 +37,10 @@ public class SecteurActiviteController {
 	@ResponseBody
 	public SecteurActivite addSecteurActivite(@RequestBody SecteurActivite sa) {
 		return secteurActiviteService.addSecteurActivite(sa);
+
 	}
 
-	// http://localhost:8089/SpringMVC/secteurActivite/remove-secteurActivite/{secteurActivite-id}
+
 	@DeleteMapping("/remove-secteurActivite/{secteurActivite-id}")
 	@ResponseBody
 	public void removeSecteurActivite(@PathVariable("secteurActivite-id") Long secteurActiviteId) {
