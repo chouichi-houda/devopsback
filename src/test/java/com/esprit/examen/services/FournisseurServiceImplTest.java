b86e83af179e3bbd682dee1e4994c0710c15e0be
unknown <rahma.chaabane@esprit.tn>
2022-11-08 15:49:39 +0000

    updatetestfournisseur

--- src/test/java/com/esprit/examen/services/FournisseurServiceImplTest.java
+++ src/test/java/com/esprit/examen/services/FournisseurServiceImplTest.java
@@ -2,6 +2,7 @@ package com.esprit.examen.services;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 import static org.mockito.Mockito.when;
 
 import java.util.Optional;
@@ -69,6 +70,15 @@ public class FournisseurServiceImplTest {
 	    	System.out.println("Retrieve operator by id works !");
 	    }
 	
+	 @Test
+		public void deleteFournisseurTest() {
+			Fournisseur f = new Fournisseur();
+			Fournisseur savedFournisseur= FourniseurService.addFournisseur(f);
+			FourniseurService.deleteFournisseur(savedFournisseur.getIdFournisseur());
+			assertNull(FourniseurService.retrieveFournisseur(savedFournisseur.getIdFournisseur()));
+		}
+
+	 
 	 
 }
 
