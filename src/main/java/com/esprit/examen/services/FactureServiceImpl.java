db1d821028d328064bc9656ba5c89fdadf077016
chaima-kaddour <79282823+chaima-kaddour@users.noreply.github.com>
2022-11-08 13:57:16 +0100

    update facture

--- src/main/java/com/esprit/examen/services/FactureServiceImpl.java
+++ src/main/java/com/esprit/examen/services/FactureServiceImpl.java
@@ -100,6 +100,7 @@ public class FactureServiceImpl implements IFactureService {
 	@Override
 	public List<Facture> getFacturesByFournisseur(Long idFournisseur) {
 		Fournisseur fournisseur = fournisseurRepository.findById(idFournisseur).orElse(null);
+		assert (fournisseur!=null);
 		return (List<Facture>) fournisseur.getFactures();
 	}
 
