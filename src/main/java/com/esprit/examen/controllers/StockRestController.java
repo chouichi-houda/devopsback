bf634f3023c0162b8c434719b80c52ea10582dac
unknown <rahma.chaabane@esprit.tn>
2022-11-08 17:22:37 +0000

    fix sonar problem

--- src/main/java/com/esprit/examen/controllers/StockRestController.java
+++ src/main/java/com/esprit/examen/controllers/StockRestController.java
@@ -44,7 +44,7 @@ public class StockRestController {
 		return stock;
 	}
 
-	// http://localhost:8089/SpringMVC/stock/remove-stock/{stock-id}
+
 	@DeleteMapping("/remove-stock/{stock-id}")
 	@ResponseBody
 	public void removeStock(@PathVariable("stock-id") Long stockId) {
@@ -70,7 +70,7 @@ public class StockRestController {
 	//@Scheduled(cron = "*/60 * * * * *")
 	//@GetMapping("/retrieveStatusStock")
 //	@ResponseBody
-//	public void retrieveStatusStock() {
+
 //		stockService.retrieveStatusStock();
 //	}
 
