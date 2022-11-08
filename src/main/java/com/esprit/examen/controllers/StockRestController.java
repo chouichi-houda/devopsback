426381a53d92bbf7e030778ac93472b505cf67c8
chaima-kaddour <79282823+chaima-kaddour@users.noreply.github.com>
2022-11-08 14:42:03 +0100

    update stock

--- src/main/java/com/esprit/examen/controllers/StockRestController.java
+++ src/main/java/com/esprit/examen/controllers/StockRestController.java
@@ -40,8 +40,8 @@ public class StockRestController {
 	@PostMapping("/add-stock")
 	@ResponseBody
 	public Stock addStock(@RequestBody Stock s) {
-		Stock stock = stockService.addStock(s);
-		return stock;
+		Stock stock = new Stock(s.getLibelleStock(),s.getQte(),s.getQteMin());
+		return stockService.addStock(stock);
 	}
 
 	// http://localhost:8089/SpringMVC/stock/remove-stock/{stock-id}
@@ -55,6 +55,7 @@ public class StockRestController {
 	@PutMapping("/modify-stock")
 	@ResponseBody
 	public Stock modifyStock(@RequestBody Stock stock) {
+		Stock stock = new Stock(s.getIdStock(),s.getLibelleStock(),s.getQte(),s.getQteMin(),s.getProduits());
 		return stockService.updateStock(stock);
 	}
 
