f0321852d83178826e55bda170b6e558f276caab
chaima-kaddour <79282823+chaima-kaddour@users.noreply.github.com>
2022-11-11 21:54:24 +0100

    fix4

--- src/main/java/com/esprit/examen/services/ReglementServiceImpl.java
+++ src/main/java/com/esprit/examen/services/ReglementServiceImpl.java
@@ -10,6 +10,8 @@ import com.esprit.examen.entities.Reglement;
 import com.esprit.examen.repositories.FactureRepository;
 import com.esprit.examen.repositories.ReglementRepository;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Service
 @Slf4j
 public class ReglementServiceImpl implements IReglementService {
