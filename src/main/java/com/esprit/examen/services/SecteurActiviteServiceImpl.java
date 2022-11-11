3593b666c64b2358b74d653f1d69105a052a5762
chaima-kaddour <79282823+chaima-kaddour@users.noreply.github.com>
2022-11-11 21:55:22 +0100

    fix4

--- src/main/java/com/esprit/examen/services/SecteurActiviteServiceImpl.java
+++ src/main/java/com/esprit/examen/services/SecteurActiviteServiceImpl.java
@@ -6,8 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.esprit.examen.entities.SecteurActivite;
 import com.esprit.examen.repositories.SecteurActiviteRepository;
+import lombok.extern.slf4j.Slf4j;
 
 @Service
+@Slf4j
 public class SecteurActiviteServiceImpl implements ISecteurActiviteService{
 
 	@Autowired
