37caf966817487d534932f8caf22a6c6b20dc101
chouichi-houda <houda.chouichi@esprit.tn>
2022-11-12 12:58:38 +0100

    try16

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
 FROM openjdk:8-jdk-alpine
-EXPOSE 8089
-ADD target/tpAchatProject-1.0.jar tpAchatProject-1.0.jar
+ARG JAR_FILE=target/*.jar
+COPY ${JAR_FILE} tpAchatProject-1.0.jar
 ENTRYPOINT ["java","-jar","/tpAchatProject-1.0.jar"]
+EXPOSE 8089
--- src/test/java/com/esprit/examen/services/ProduitServiceImplTest.java
+++ src/test/java/com/esprit/examen/services/ProduitServiceImplTest.java
@@ -15,10 +15,13 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import com.esprit.examen.entities.Produit;
+import com.esprit.examen.entities.Stock;
 import com.esprit.examen.repositories.ProduitRepository;
+import com.esprit.examen.repositories.StockRepository;
 //@SpringBootTest
 @ExtendWith(SpringExtension.class)
 class ProduitServiceImplTest {
@@ -27,29 +30,23 @@ class ProduitServiceImplTest {
 	@InjectMocks
 	ProduitServiceImpl produitService;
 	
+	@InjectMocks
+	StockServiceImpl stockService;
+	
 	DateFormat df = new SimpleDateFormat("MM-dd-yyyy"); 
 	
 	
     @Mock
     private ProduitRepository produitRepository;
+    private StockRepository stockRepository;
     
     private Produit produit1 = new Produit(null,"777434","libbPorduit", 10, new Date(), new Date(),null,null,null);
     private Produit produit2 = new Produit(null,"777434","libbPorduit", 10, new Date(), new Date(),null,null,null);
 	
-	
-	@Test 
-	void addProductTest() {
-    	when(produitRepository.save(produit1)).thenReturn(produit1);
-    	assertNotNull(produit1);
-    	
-    	Produit persisted = produitService.addProduit(produit1);
-		assertEquals(produit1, persisted); 
-    	
-		System.out.println("add product works !");
-	} 
-	
-	 @Test 
-	 void retrieveaallProductTest() {
+    private Stock stock = new Stock(Long.valueOf(1), "f1", 10, 5, null);
+    
+    @Test 
+	 void testRetrieveAllProduits() {
 	    	when(produitRepository.findAll()).thenReturn(Stream
 	    			.of(produit1,produit2)
 	    			.collect(Collectors.toList()));
@@ -57,9 +54,29 @@ class ProduitServiceImplTest {
 	    	assertEquals(2,produitService.retrieveAllProduits().size());
 	    	System.out.println("Retrieve all Produit works !");
 	    }
+    
+	@Test 
+	void testAddProduit() {
+    	when(produitRepository.save(produit1)).thenReturn(produit1);
+    	assertNotNull(produit1);
+    	
+    	Produit p = produitService.addProduit(produit1);
+		assertEquals(produit1, p); 
+    	
+		System.out.println("add product works !");
+	} 
+	
+	  @Test 
+	   void testDeleteProduit() {
+		  produitService.deleteProduit(produit1.getIdProduit());
+		  Mockito.verify(produitRepository).deleteById(produit1.getIdProduit());
+		  System.out.println("deleted");
+	    }
+	
+	
 	
 	   @Test 
-	   void UpdateProductTest() {
+	   void testUpdateProduit() {
 	    	when(produitRepository.save(produit1)).thenReturn(produit1);
 	    	assertNotNull(produit1);
 	    	assertEquals(produit1, produitService.updateProduit(produit1));
@@ -67,11 +84,22 @@ class ProduitServiceImplTest {
 	    }
 	    
 	    @Test
-	    void retrieveProductTest() {
+	    void testRetrieveProduit() {
 	    	when(produitRepository.findById(produit1.getIdProduit())).thenReturn(Optional.of(produit1));
 	    	assertEquals(produit1, produitService.retrieveProduit(produit1.getIdProduit()));
 	    	System.out.println("Retrieve product by id works !");
 	    }
+	    
+//	    @Test 
+//		   void testAssignProduitToStock() {
+//	    	when(produitRepository.findById(produit1.getIdProduit())).thenReturn(Optional.of(produit1));
+//	    	assertEquals(produit1, produitService.retrieveProduit(produit1.getIdProduit()));
+//	    	when(stockRepository.findById(stock.getIdStock())).thenReturn(stock);
+//			  assertNotNull(produit1);
+//			  ((ProduitServiceImpl) Mockito.verify(produitRepository)).assignProduitToStock(produit1.getIdProduit(),stock.getIdStock());
+//			  System.out.println("deleted");
+//		    }
+	  
 
     
 	
