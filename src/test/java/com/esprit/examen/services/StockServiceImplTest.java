51336a15514b92144b00cca76d560a1f0ea02d49
Amal-Soltani <79099904+Amal-Soltani@users.noreply.github.com>
2022-11-14 16:14:29 +0100

    Update StockServiceImplTest.java

--- src/test/java/com/esprit/examen/services/StockServiceImplTest.java
+++ src/test/java/com/esprit/examen/services/StockServiceImplTest.java
@@ -18,7 +18,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import com.esprit.examen.entities.Stock;
 import com.esprit.examen.repositories.StockRepository;
 
-@SpringBootTest(classes = com.esprit.examen.class)
+@SpringBootTest(classes =TpAchatProjectApplication.class)
 @RunWith(MockitoJUnitRunner.class)
 public class StockServiceImplTest {
 
